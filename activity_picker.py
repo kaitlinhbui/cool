21b3b0f4d78d2d738528af24a2e0ce61d18e8ab8
kaitlinhbui <kaitlinhb01@gmail.com>
2016-08-26 16:56:55 -0700

    Create activity_picker.py

---
+++ activity_picker.py
@@ -0,0 +1,8 @@
+# import special libraries already built in python
+
+# list of options to select from
+possible_activities = []
+
+# choice of what we are going to do
+
+# display the resuts to the ends user
