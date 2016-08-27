8367dedad86411e04428daf2a20f52c50baf1816
kaitlinhbui <kaitlinhb01@gmail.com>
2016-08-26 17:47:02 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,11 +1,17 @@
 # import special libraries already built in python
 import random
+import urllib2
 
 # list of options to select from
-possible_activities = ['a', 'b', 'c', 'd', 'e', 'f']
+url = "https://raw.githubusercontent.com/kaitlinhbui/cool/master/activities.list"
+list_raw_text = urllib2.urlopen(url).read()
+
+# print "DEBUG: " + str(list_raw_text.split())
+
+possible_activities = list_raw_text.split()
 
 # choice of what we are going to do
-choice = possible_activities[random.randint(0, 5)]
+choice = random.choice(possible_activities)
 
 # display the resuts to the ends user
 print("Possible activities are: " + str(possible_activities))
